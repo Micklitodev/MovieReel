9452f3a3f63eb1599f6c79c1d2d22e2e363e45b7
[mick lito] <[micklito.dev@gmail.com]>
2023-02-02 14:44:05 -0500

    updated main.js so that renderGenre function also creates a genKey arr and pushes to it the responses keys, then sets the attribute of value to the gen key in the loop for each button. changed handle click to take this new updated function feature by changing searchGenreApi argument call from (e.target.textContent to e.target.value)

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+
+
 const endpoint = "https://api.themoviedb.org/3";
 const apiKey = "3ec796860d338dbe182981a1c1f3f1c1";
 
@@ -17,19 +19,47 @@ const apiEngine = async () => {
   }
 };
 
+let genKeyArr = []
+
 const renderGenre = async () => {
   const jsonResponse = await apiEngine();
   var genreArr = [];
+  genKeyArr.push(Object.keys(jsonResponse.genres))
   for (let i = 0; i < 19; i++) {
     genreArr.push(jsonResponse.genres[i].name);
   }
   for (let i = 0; i < 19; i++) {
     const btnEl = document.createElement("button");
     btnEl.textContent = genreArr[i];
+    btnEl.setAttribute('value', genKeyArr[0][i])
     document.body.appendChild(btnEl);
+    btnEl.addEventListener('click', handleClick)
   }
 };
 
 
+function handleClick(e) {
+  e.preventDefault()
+  searchGenreApi(e.target.value)
+  removeEventListener('click', handleClick)
+}
+
+const searchGenreApi = async (genre) => {
+  const path = "/discover/movie";
+  const queryParams = `?certification_country=US&with_genres=${genre}&sort_by=vote_average.desc&api_key=${apiKey}`;
+  console.log(queryParams)
+  const url = `${endpoint}${path}${queryParams}`;
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      console.log(jsonResponse)
+    }
+  } catch (Error) {
+    console.log(Error);
+  }
+
+}
 
 renderGenre();
+

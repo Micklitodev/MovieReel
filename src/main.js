bc0127cb6b1b39b7d0ad7aac0b45b78ffcc65dbd
[mick lito] <[micklito.dev@gmail.com]>
2023-02-02 02:22:40 -0500

    updated index.js so that api returns json data then renderGenre pushes it to an array and appends button elements with indexed array text content

--- src/main.js
+++ src/main.js
@@ -1,22 +1,35 @@
-const endpoint = 'https://www.google.com/'
-const apiKey = '*hasidufhD-08md09spau8mr4'
+const endpoint = "https://api.themoviedb.org/3";
+const apiKey = "3ec796860d338dbe182981a1c1f3f1c1";
 
 
 const apiEngine = async () => {
-   const path = 'test/api'
-   const queryParams = `?my_api=${apiKey}`
-   const url = `${endpoint}${path}${queryParams}`
-   
-   try{
-    const response = await fetch(url)
+  const path = "/genre/movie/list";
+  const queryParams = `?api_key=${apiKey}`;
+  const url = `${endpoint}${path}${queryParams}`;
+  try {
+    const response = await fetch(url);
     if (response.ok) {
-        const jsonResponse = response.json()
-        return jsonResponse
+      const jsonResponse = await response.json();
+      return jsonResponse;
     }
+  } catch (Error) {
+    console.log(Error);
+  }
+};
 
-   }catch(Error) {
-    console.log(Error)
-   }
-}           
+const renderGenre = async () => {
+  const jsonResponse = await apiEngine();
+  var genreArr = [];
+  for (let i = 0; i < 19; i++) {
+    genreArr.push(jsonResponse.genres[i].name);
+  }
+  for (let i = 0; i < 19; i++) {
+    const btnEl = document.createElement("button");
+    btnEl.textContent = genreArr[i];
+    document.body.appendChild(btnEl);
+  }
+};
 
 
+
+renderGenre();

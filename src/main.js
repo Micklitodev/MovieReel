52f50156a49847fece716fbf4487ff1835962d11
[mick lito] <[micklito.dev@gmail.com]>
2023-02-03 02:01:32 -0500

    updated the main.js to render an image, an atag link wrapping an image, and a Ptag. Fixed formating of index.html so that is better organized and easier to manipulate added id/class tags to index.html. Updated css file to better display the content in a more neat fashion and manipulate js appended and rendered elements to follow the UI flow. app has a good base start for everyone to work of off.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const main = document.getElementById("main");
+
   const submitBtn = document.querySelector(".btn");
   const formEl = document.getElementById("name");
 
@@ -7,7 +9,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const tmdbEndPoint = "https://api.themoviedb.org/3";
   const tmdbApiKey = "3ec796860d338dbe182981a1c1f3f1c1";
-
 
   const searchNameEngine = async (genre) => {
     const path = "/search/movie";
@@ -40,49 +41,64 @@ document.addEventListener("DOMContentLoaded", function () {
     renderData();
   }
 
+  const motnEndPoint =
+    "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com";
 
-  const motnEndPoint = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'
-
-  const searchAvalibilityEngine = async (name) => { 
-    
+  const searchAvalibilityEngine = async (name) => {
     const options = {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'X-RapidAPI-Key': '5e25bf14aamsh93f348e1f56f1f0p1162c9jsnc01051fd8308',
-        'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'
-      }
+        "X-RapidAPI-Key": "5e25bf14aamsh93f348e1f56f1f0p1162c9jsnc01051fd8308",
+        "X-RapidAPI-Host":
+          "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+      },
     };
-    
-  
-   
-    name = name.split(" ").join("")
-    console.log(name)
+
+    name = name.split(" ").join("");
+    console.log(name);
     const path = "/lookup";
     const queryParams = `?term=${name}&country=us`;
     const url = `${motnEndPoint}${path}${queryParams}`;
-    console.log(url)
+    console.log(url);
     try {
       const response = await fetch(url, options);
       if (response.ok) {
         const jsonResponse = await response.json();
-        console.log(jsonResponse)
-        console.log(jsonResponse.results[0].locations[3].display_name)
-        let displayName = jsonResponse.results[0].locations[3].display_name
-        if(displayName) {
-          pEl = document.createElement('p')
-          pEl.textContent = `This show is on ${displayName}`
-          document.body.append(pEl)
+        console.log(jsonResponse);
+        console.log(jsonResponse.results[0].locations[3].display_name);
+        let displayName = jsonResponse.results[0].locations[3].display_name;
+        if (displayName) {
+          let link = jsonResponse.results[0].locations[0].url;
+          let icon = jsonResponse.results[0].locations[0].icon;
+          let img = jsonResponse.results[0].picture;
+
+          let pEl = document.createElement("p");
+          let aEl = document.createElement("a");
+          let imgEl = document.createElement("img");
+          let iframe = document.createElement("img");
+
+          main.appendChild(iframe);
+          main.appendChild(pEl);
+          main.appendChild(aEl);
+          aEl.appendChild(imgEl);
+
+          imgEl.setAttribute("src", `${icon}`);
+          aEl.setAttribute("href", `${link}`);
+          iframe.setAttribute("src", `${img}`);
+
+          pEl.setAttribute("class", "rendered-container1");
+          aEl.setAttribute("class", "rendered-container");
+          imgEl.setAttribute("class", "rendered-container");
+          iframe.setAttribute("class", "rendered-container2");
+          iframe.style.cssText = "max-width: 300px";
+
+          pEl.textContent = `Watch now on:`;
         }
       }
     } catch (Error) {
       console.log(Error);
     }
   };
-  
- searchAvalibilityEngine()
+
+  searchAvalibilityEngine();
 });
-
-
-
-
-
